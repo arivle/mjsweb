4370b8583b22cd83a103b633f6404c46140e7dc2
arivle <arivleone@gmail.com>
2018-07-02 12:37:07 +0700

    fixing the minor problem of views

--- application/views/kas.php
+++ application/views/kas.php
@@ -38,130 +38,128 @@
 <body class="animsition">
     <div class="page-wrapper">
         <!-- HEADER MOBILE-->
-        <header class="header-mobile d-block d-lg-none">
-            <div class="header-mobile__bar">
-                <div class="container-fluid">
-                    <div class="header-mobile-inner">
-                        <a class="logo" href="index.html">
-                            <img src="<?php echo base_url();?>assets/images/icon/logo.png" alt="CoolAdmin" />
-                        </a>
-                        <button class="hamburger hamburger--slider" type="button">
+       <header class="header-mobile d-block d-lg-none">
+        <div class="header-mobile__bar">
+            <div class="container-fluid">
+                <div class="header-mobile-inner">
+                    <a class="logo" href="index.html">
+                        <img src="<?php echo base_url();?>assets/images/icon/logo.png" alt="CoolAdmin"/>
+                    </a>
+                    <button class="hamburger hamburger--slider" type="button">
                             <span class="hamburger-box">
                                 <span class="hamburger-inner"></span>
                             </span>
-                        </button>
-                    </div>
+                    </button>
                 </div>
             </div>
-            <nav class="navbar-mobile">
-                <div class="container-fluid">
-                    <ul class="navbar-mobile__list list-unstyled">
-                        <li class="active has-sub">
-                            <a class="js-arrow" href="beranda.html">
-                                <i class="fas fa-tachometer-alt"></i>Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="absensi.html">
-                                <i class="fas fa-calendar-alt"></i>Absensi</a>
-                        </li>
-                        <li>
-                            <a href="absensi.html">
-                                <i class="fas fa-user"></i>Pegawai</a>
-                        </li>
-                        <li>
-                            <a href="form.html">
-                                <i class="far fa-check-square"></i>Daftar Proyek</a>
-                        </li>
-                        <li class="active has-sub">
-                            <a class="js-arrow" href="#">
-                                <i class="fas fa-dollar"></i>Keuangan</a>
-                            <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
-                                <li>
-                                    <a href="table.html">
-                                        <i class="fas fa-briefcase"></i>Kas</a>
-                                </li>
-                                <li>
-                                    <a href="table.html">
-                                        <i class="fas fa-download"></i>Pemasukan Proyek</a>
-                                </li>
-                                <li>
-                                    <a href="table.html">
-                                        <i class="fas fa-upload"></i>Pengeluaran Proyek</a>
-                                </li>
-                                <li>
-                                    <a href="chart.html">
-                                        <i class="fas fa-chart-bar"></i>Laporan Pemasukan</a>
-                                </li>
-                                <li>
-                                    <a href="chart.html">
-                                        <i class="fas fa-chart-bar"></i>Laporan Pengeluaran</a>
-                                </li>
+        </div>
+        <nav class="navbar-mobile">
+            <div class="container-fluid">
+                <ul class="navbar-mobile__list list-unstyled">
+                    <li>
+                        <a href="<?php echo base_url();?>Welcome"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                    </li>
+                    <li >
+                        <a href="<?php echo base_url();?>Absensi">
+                            <i class="fas fa-calendar-alt"></i>Absensi</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url();?>Pegawai">
+                            <i class="fas fa-user"></i>Pegawai</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="far fa-check-square"></i>Daftar Proyek</a>
+                    </li>
+                    <li class="active has-sub">
+                        <a class="js-arrow" href="#">
+                            <i class="fas fa-dollar"></i>Keuangan</a>
+                        <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                            <li class="active has-sub">
+                                <a class="js-arrow" href="<?php echo base_url();?>Kas">
+                                    <i class="fas fa-briefcase"></i>Kas</a>
+                            </li>
+                            <li>
+                                <a href="<?php echo base_url();?>Pemasukan">
+                                    <i class="fas fa-download"></i>Pemasukan Proyek</a>
+                            </li>
+                            <li>
+                                <a href="<?php echo base_url();?>Pengeluaran">
+                                    <i class="fas fa-upload"></i>Pengeluaran Proyek</a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fas fa-chart-bar"></i>Laporan Pemasukan</a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fas fa-chart-bar"></i>Laporan Pengeluaran</a>
+                            </li>
 
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <!-- END HEADER MOBILE-->
+
+    <!-- MENU SIDEBAR-->
+    <aside class="menu-sidebar d-none d-lg-block">
+        <div class="logo">
+            <a href="#">
+                <img src="<?php echo base_url();?>assets/images/icon/logo.png" alt="Cool Admin"/>
+            </a>
+        </div>
+        <div class="menu-sidebar__content js-scrollbar1">
+            <nav class="navbar-sidebar">
+                <ul class="list-unstyled navbar__list">
+                    <li >
+                        <a href="<?php echo base_url();?>Welcome"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                    </li>
+                    <li class="active has-sub">
+                        <a class="js-arrow" href="<?php echo base_url();?>Absensi">
+                            <i class="fas fa-calendar-alt"></i>Absensi</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url();?>Pegawai">
+                            <i class="fas fa-user"></i>Pegawai</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="far fa-check-square"></i>Daftar Proyek</a>
+                    </li>
+                    <li class="active has-sub">
+                        <a class="js-arrow" href="#">
+                            <i class="fas fa-dollar"></i>Keuangan</a>
+                        <ul class="list-unstyled navbar__sub-list js-sub-list">
+                            <li class="active has-sub">
+                                <a class="js-arrow" href="<?php echo base_url();?>Kas">
+                                    <i class="fas fa-briefcase"></i>Kas</a>
+                            </li>
+                            <li>
+                                <a href="<?php echo base_url();?>Pemasukan">
+                                    <i class="fas fa-download"></i>Pemasukan Proyek</a>
+                            </li>
+                            <li>
+                                <a href="<?php echo base_url();?>Pengeluaran">
+                                    <i class="fas fa-upload"></i>Pengeluaran Proyek</a>
+                            </li>
+                            <li>
+                                <a href="chart.html">
+                                    <i class="fas fa-chart-bar"></i>Laporan Pemasukan</a>
+                            </li>
+                            <li>
+                                <a href="chart.html">
+                                    <i class="fas fa-chart-bar"></i>Laporan Pengeluaran</a>
+                            </li>
+
+                        </ul>
+                    </li>
+                </ul>
             </nav>
-        </header>
-        <!-- END HEADER MOBILE-->
-
-        <!-- MENU SIDEBAR-->
-        <aside class="menu-sidebar d-none d-lg-block">
-            <div class="logo">
-                <a href="#">
-                    <img src="<?php echo base_url();?>assets/images/icon/logo.png" alt="Cool Admin" />
-                </a>
-            </div>
-            <div class="menu-sidebar__content js-scrollbar1">
-                <nav class="navbar-sidebar">
-                    <ul class="list-unstyled navbar__list">
-                        <li class="active has-sub">
-                            <a class="js-arrow" href="beranda.html">
-                                <i class="fas fa-tachometer-alt"></i>Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="absensi.html">
-                                <i class="fas fa-calendar-alt"></i>Absensi</a>
-                        </li>
-                        <li>
-                            <a href="absensi.html">
-                                <i class="fas fa-user"></i>Pegawai</a>
-                        </li>
-                        <li>
-                            <a href="form.html">
-                                <i class="far fa-check-square"></i>Daftar Proyek</a>
-                        </li>
-                        <li class="active has-sub">
-                            <a class="js-arrow" href="#">
-                                <i class="fas fa-dollar"></i>Keuangan</a>
-                            <ul class="list-unstyled navbar__sub-list js-sub-list">
-                                <li>
-                                    <a href="table.html">
-                                        <i class="fas fa-briefcase"></i>Kas</a>
-                                </li>
-                                <li>
-                                    <a href="table.html">
-                                        <i class="fas fa-download"></i>Pemasukan Proyek</a>
-                                </li>
-                                <li>
-                                    <a href="table.html">
-                                        <i class="fas fa-upload"></i>Pengeluaran Proyek</a>
-                                </li>
-                                <li>
-                                    <a href="chart.html">
-                                        <i class="fas fa-chart-bar"></i>Laporan Pemasukan</a>
-                                </li>
-                                <li>
-                                    <a href="chart.html">
-                                        <i class="fas fa-chart-bar"></i>Laporan Pengeluaran</a>
-                                </li>
-
-                            </ul>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </aside>
+        </div>
+    </aside>
         <!-- END MENU SIDEBAR-->
 
         <!-- PAGE CONTAINER-->
