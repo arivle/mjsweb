c91b8cd3d7c998413e44a17755621f4862c377c1
Dwi Wahyu <bxmailo@gmail.com>
2018-07-02 22:44:02 +0700

    view tabel pegawai
    masih nama pegawai

--- application/views/pegawai.php
+++ application/views/pegawai.php
@@ -395,9 +395,13 @@
                                     </tr>
                                     </thead>
                                     <tbody>
+									<?php
+									$i = 0;
+									foreach($pegawai as $p){
+										$i++; ?>
                                     <tr class="tr-shadow">
-                                        <td>1.</td>
-                                        <td>Loadhi Didi</td>
+                                        <td><?= $i.'.'; ?></td>
+                                        <td><?= $p->namaPegawai ?></td>
                                         <td>
                                             <span class="role admin">admin</span>
                                         </td>
@@ -422,6 +426,7 @@
                                         </td>
                                     </tr>
                                     <tr class="spacer"></tr>
+									<?php } ?>
                                     </tbody>
                                 </table>
                             </div>
@@ -538,34 +543,3 @@
         <!-- end modal create -->
 
     </div>
-
-</div>
-
-<!-- Jquery JS-->
-<script src="vendor/jquery-3.2.1.min.js"></script>
-<!-- Bootstrap JS-->
-<script src="vendor/bootstrap-4.1/popper.min.js"></script>
-<script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
-<!-- Vendor JS       -->
-<script src="vendor/slick/slick.min.js">
-</script>
-<script src="vendor/wow/wow.min.js"></script>
-<script src="vendor/animsition/animsition.min.js"></script>
-<script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
-</script>
-<script src="vendor/counter-up/jquery.waypoints.min.js"></script>
-<script src="vendor/counter-up/jquery.counterup.min.js">
-</script>
-<script src="vendor/circle-progress/circle-progress.min.js"></script>
-<script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
-<script src="vendor/chartjs/Chart.bundle.min.js"></script>
-<script src="vendor/select2/select2.min.js">
-</script>
-
-<!-- Main JS-->
-<script src="js/main.js"></script>
-
-</body>
-
-</html>
-<!-- end document-->
