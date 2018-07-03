6a4322e56c6d49323d3dbc1d8f72966b05384461
Dwi Wahyu <bxmailo@gmail.com>
2018-07-03 16:19:37 +0700

    +model klien

---
+++ application/models/M_klien.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bxmai
+ * Date: 7/3/2018
+ * Time: 4:16 PM
+ */
+
+class M_klien
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	function getAll()
+	{
+		return $this->db->get('klien');
+	}
+
+	function getKlien($idKlien)
+	{
+		return $this->db->get_where('klien', array('idKlien' => $idKlien));
+	}
+
+	function addKlien($data)
+	{
+		$this->db->insert('klien', $data);
+	}
+
+	function updateKlien($where, $data)
+	{
+		$this->db->where($where);
+		$this->db->update('klien', $data);
+	}
+
+	function hapusKlien($where)
+	{
+		$this->db->where($where);
+		$this->db->delete('klien');
+	}
+}
