be03d77994d7ae66b9778e8ee421df7bfaad7d5e
Sofyan Sugianto <sofyanartem@gmail.com>
2018-07-04 20:24:02 +0700

    change route to pegawai

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'login';
+$route['default_controller'] = 'pegawai';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['logout']='login/logout';
+$route['logout']='pegawai/logout';
+$route['login']='pegawai/login';
